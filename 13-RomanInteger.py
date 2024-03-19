295cedb50b4b6340bb3864071fa0b78313f0cb17
Keanu Rico Sida <keanu.sida@gmail.com>
2024-03-18 23:42:55 -0400

    Completed RomanInteger exercise.

--- 13-RomanInteger.py
+++ 13-RomanInteger.py
@@ -1,5 +1,6 @@
 class Solution:
     def romanToInt(s) -> int:
+        # Initalize dictionary of values for each symbol.
         values = {
             "I": 1,
             "V": 5,
@@ -9,5 +10,12 @@ class Solution:
             "D": 500,
             "M": 1000
         }
+        # Replace each subtractive element in the string with its addition-exclusive equivalent.
+        # Care is taken here to do low replacements first, since they can create additional higher subtractive elements, but not vice versa.
         s = s.replace("IV", "IIII").replace("IX", "VIIII")
-        s = s.replace("XL", "LXXX")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        # Iterate over the string, adding each element's value to the total.
+        for char in s:
+            total += values[char]
+        return total
